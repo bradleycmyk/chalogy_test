f40b2ef2da54f9876f1982b91d55e6c65aa7f5cc
Sean <sean.sehwan.choi@gmail.com>
2016-06-22 17:03:05 -0400

    displaying seperated search result for pages and products, and function to highlight search word

--- client/js/search/search.controller.js
+++ client/js/search/search.controller.js
@@ -3,38 +3,53 @@ app.controller('searchController', function($scope, ProductFactory, $state, $sta
 	var text = $stateParams.text;
 	var keyword = $stateParams.text;
 
-	console.log("text: ", text);
-	console.log("keyword: ", keyword);
+	$scope.alertmsg = "WE ARE SORRY. There are no items to match your search, please rephrase and try again.";
+	$scope.suggestion = ["Make sure all words are spelled correctly.", "Try different keywords.", "Try more general keywords"];
+	$scope.imgurl = "search-again.jpg";
 
+	// product search 
 	if (text == ""){
 		console.log("do nothing");
 	} else {
 		ProductFactory.searchDb(text).then(function(saturdayData) {
 		
 			if(saturdayData.length === 0) {
-				$scope.alertmsg = "WE ARE SORRY. There are no items to match your search, please rephrase and try again.";
-				$scope.suggestion = ["Make sure all words are spelled correctly.", "Try different keywords.", "Try more general keywords"];
-				$scope.imgurl = "search-again.jpg";
+				$(".alert-msg").css({"display":"block"});
 				$(".giphy").show();
 			}
 			$scope.products = saturdayData;
 		})
 	}
 
+	// page search 
 	if (keyword == ""){
 		console.log("no keywords");
 	} else {
 		ProductFactory.searchPagesDb(keyword).then(function(pages) {
-		
-			// if(pages.length === 0) {
-			// 	$scope.alertmsg = "WE ARE SORRY. There are no items to match your search, please rephrase and try again.";
-			// 	$scope.suggestion = ["Make sure all words are spelled correctly.", "Try different keywords.", "Try more general keywords"];
-			// 	$scope.imgurl = "search-again.jpg";
-			// 	$(".giphy").show();
-			// }
 			$scope.pages = pages;
 		})
 	}
+
+
+	// highligting search text !! 
+
+	$(document).ready(function(){
+
+	    function highlightSearchWord() {
+		    var text = $stateParams.text;
+		    var query = new RegExp("(\\b" + text + "\\b)", "gim");
+
+		    for(var i = 0; i < $(".pages").length; i++) {
+		    	var e = $(".pages")[i].innerHTML;
+		    	var enew = e.replace(/(<span>|<\/span>)/igm, "");
+			    $(".pages")[i].innerHTML = enew;
+			    var newe = enew.replace(query, "<span>$1</span>");
+			    $(".pages")[i].innerHTML = newe;
+		    }
+		}
+		setTimeout(highlightSearchWord, 1500);
+
+	})
 
 });
 
