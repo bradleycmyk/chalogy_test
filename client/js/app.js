e5f8fd4ea39134d7ff618d7c34004a00a1e89446
Sean <sean.sehwan.choi@gmail.com>
2016-06-27 15:26:52 -0400

    added full page js to sat-brand page for testing purpose, will continue to work on animation

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('Chalogy', ['ui.router']);
 // var app = angular.module('Chalogy', ['ui.router', 'authHandler']);
 
-app.controller('MainController', function($scope, $rootScope, $window, $state){
+app.controller('MainController', function($scope, $rootScope, $window, $state, $timeout){
 	$scope.loggedIn = sessionStorage.loggedinUser || undefined;
 
 	$scope.isActiveFor = function (route) {
@@ -14,6 +14,41 @@ app.controller('MainController', function($scope, $rootScope, $window, $state){
 		} else {
 			// console.log("something is wrong");
 		}
+	}
+
+	$timeout(function() {console.log("set time out function called ")}, 2000);
+
+	// $scope.isSecondPage = function () {
+	// 	if ($window.location.href.split("brand#")[1] == "secondPage") {
+	// 		document.getElementById("vid1").load();
+	// 		document.getElementById("vid1").play();
+	// 	}
+	// };
+
+	// window.onload = function() {
+ //    	if(!window.location.hash) {
+ //        	window.location = window.location + '#loaded';
+ //        	window.location.reload();
+ //    	}
+	// }
+	
+	// $(document).ready(function(){    
+	//     //Check if the current URL contains '#'
+	//     if(document.URL.indexOf("#")==-1){
+	//         // Set the URL to whatever it was plus "#".
+	//         url = document.URL+"#";
+	//         location = "#";
+
+	//         //Reload the page
+	//         location.reload(true);
+	//     }
+	// });
+
+	window.onload = function () {
+	    if (! localStorage.justOnce) {
+	        localStorage.setItem("justOnce", "true");
+	        window.location.reload();
+	    }
 	}
 	
 })
