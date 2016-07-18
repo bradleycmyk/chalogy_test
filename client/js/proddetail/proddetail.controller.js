29c78e2659e3150d8be26e289456deabb4f22740
Sean <sean.sehwan.choi@gmail.com>
2016-07-18 11:45:09 -0400

    for github testing

--- client/js/proddetail/proddetail.controller.js
+++ client/js/proddetail/proddetail.controller.js
@@ -13,10 +13,37 @@ app.controller('ProdDetailCtrl', function($scope, $http, $state, $stateParams, P
 		$scope.tab = setTab;
 	};
 
+	// shopify info 
+	    var shopClient = ShopifyBuy.buildClient({
+			apiKey: 'f7558dd14f7c9c3289b1bd175989fd5f',
+			domain: 'www.designplusshop.com/',
+			appId: '6'
+		});
+	
+	    // Little D + Tiny Emoji Stickers, Volume 2
+	    shopClient.fetchProduct(1933592772)
+		  .then(function (product) {
+		    console.log(product);
+		  })
+		  .catch(function () {
+		    console.log('Request failed');
+		  });
+
+		var cart;
+		shopClient.createCart().then(function (newCart) {
+		  cart = newCart;
+		  console.log("cart: ",cart);
+		  // do something with updated cart
+		});
+	
 	$(document).ready(function(){
+
 		setTimeout(function() {
 	      $('#white-wrap-d').removeClass('white-wrap')
 	    }, 250);
+
+
+
 	})
 
 });
